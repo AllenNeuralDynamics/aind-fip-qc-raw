fix(error investigation kh (#15)): Adds a suggen change check using max derivatives
* visualization optimize + rig/experimenter info

* "No sudden changes in signals" added

code/run_capsule.py
@@ -67,7 +67,7 @@ def generate_metrics(
     CMOSFloorDark_Green_Limit = 265
     CMOSFloorDark_Iso_Limit = 265
     CMOSFloorDark_Red_Limit = 265
-    sudden_change_limit = 5000
+    sudden_change_limit = 2000
     metrics = {
         "IsDataSizeSame": len(data1) == len(data2) == len(data3),
         "IsDataLongerThan15min": len(data1) > 18000,
@@ -107,24 +107,35 @@ def create_evaluation(
     )
 
 
-def plot_cmos_trace_data(data_list, colors, results_folder):
+def plot_cmos_trace_data(data_list, colors, results_folder, rig_id, experimenter):
     """Plot raw frame and cmos data and save to a file."""
     data1 = data_list[0]
     data2 = data_list[1]
-    plt.figure(figsize=(8, 4))
+    plt.figure(figsize=(10, 16))
     for i_panel in range(4):
         plt.subplot(8, 1, i_panel + 1)
         plt.plot(data1[:, i_panel + 1], color=colors[0])
-        plt.title("GreenCh ROI:" + str(i_panel))
-        plt.xlabel("frames")
-        plt.ylabel("CMOS pixel val")
+        if i_panel == 0:
+            plt.title(
+                "GreenCh ROI:"
+                + str(i_panel)
+                + " rig: "
+                + rig_id
+                + " by: "
+                + experimenter
+            )
+            plt.ylabel("CMOS pixel val")
+        else:
+            plt.title("GreenCh ROI:" + str(i_panel))
 
     for i_panel in range(4):
         plt.subplot(8, 1, i_panel + 5)
         plt.plot(data2[:, i_panel + 1], color=colors[1])
         plt.title("RedCh ROI:" + str(i_panel))
-        plt.xlabel("frames")
-        plt.ylabel("CMOS pixel val")
+    plt.xlabel("frames")
+
+    plt.subplots_adjust(hspace=1.2)
+
     plt.savefig(f"{results_folder}/raw_traces.png")
     plt.savefig(f"{results_folder}/raw_traces.pdf")
     plt.show()
@@ -189,14 +200,14 @@ def plot_sync_pulse_diff(rising_time, results_folder):
     diffs = np.diff(rising_time)
 
     # Create the plot
-    plt.figure()
+    plt.figure(figsize=(2, 2))
     plt.hist(diffs, bins=100, range=(0, 0.2))
     plt.title("sync pulse diff")
     plt.ylabel("counts")
     plt.xlabel("ms")
 
     # Save and show the plot
-    plt.savefig(f"{results_folder}/SyncPulseDiff.png")
+    plt.savefig(f"{results_folder}/SyncPulseDiff.png", dpi=300, bbox_inches="tight")
     plt.savefig(f"{results_folder}/SyncPulseDiff.pdf")
     plt.show()
 
@@ -222,6 +233,10 @@ def main():
     data_disc_json = load_json_file(fiber_base_path / "data_description.json")
     asset_name = data_disc_json.get("name")
     setup_logging("aind-fip-qc-raw", mouse_id=subject_id, session_name=asset_name)
+
+    session_data = load_json_file(fiber_base_path / "session.json")
+    rig_id = session_data.get("rig_id")
+    experimenter = session_data.get("experimenter_full_name")[0]
 
     # Assuming `fiber_raw_path` is defined earlier in the code
     fiber_channel_patterns = ["FIP_DataG*", "FIP_DataIso_*", "FIP_DataR_*"]
@@ -271,6 +286,8 @@ def main():
             data_list=[data1, data2],
             colors=["darkgreen", "magenta"],
             results_folder=results_folder,
+            rig_id=rig_id,
+            experimenter=experimenter,
         )
         plot_sensor_floor(data1, data2, data3, results_folder)
         plot_sync_pulse_diff(rising_time, results_folder)
@@ -400,17 +417,42 @@ def main():
                     ),
                 ],
             ),
+            create_evaluation(
+                "No sudden changes in signals",
+                "Pass when no sudden change in signal",
+                [
+                    QCMetric(
+                        name="Max 1st derivative",
+                        value=float(
+                            np.max(
+                                [
+                                    np.max(np.diff(data1[10:-2, 1])),
+                                    np.max(np.diff(data2[10:-2, 1])),
+                                    np.max(np.diff(data3[10:-2, 1])),
+                                ]
+                            )
+                        ),
+                        status_history=[
+                            Bool2Status(
+                                metrics["NoSuddenChangeInSignal"],
+                                t=datetime.now(seattle_tz),
+                            )
+                        ],
+                        reference=str(ref_folder / "raw_traces.png"),
+                    ),
+                ],
+            ),
         ]
 
         # Create QC object and save
         qc = QualityControl(evaluations=evaluations)
         qc.write_standard_file(output_directory=str(results_folder))
 
-        # We'd like to have our files organized such that QC is in the 
+        # We'd like to have our files organized such that QC is in the
         # results directory while plots are in a named folder.
         # This allows the final results asset to have the same structure
-        # We need to generate QC in the parent to ensure it works with the 
-        # Web portal 
+        # We need to generate QC in the parent to ensure it works with the
+        # Web portal
         excluded_file = "quality_control.json"
         # Iterate over files in the results directory
         for filename in os.listdir(results_folder):